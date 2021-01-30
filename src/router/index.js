6aa11eaefe08dc280e92a9c4bc91c23ccd054421
kristo.josip <kristo.josip@nsoft.ba>
2021-01-30 10:59:27 +0100

    Adjust config to vue 2

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/layouts/TemplateDefault';
 
-export default createRouter({
-  history: createWebHashHistory(),
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: () => import(/* webpackChunkName: "home" */ '@/pages/Home'),
+      component: Home,
     },
   ],
 });
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,7 @@
-import { createStore } from 'vuex';
+import Vue from 'vue';
+import Vuex from 'vuex';
 import storeConfig from './storeConfig';
 
-export default createStore(storeConfig);
+Vue.use(Vuex);
+
+export default new Vuex.Store(storeConfig);
